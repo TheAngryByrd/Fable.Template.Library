09579aee1fd1e53ce80210f9f1d06e1918b4d88d
Jimmy Byrd <jimmy.byrd@binarydefense.com>
2017-09-08 21:07:17 -0400

    Ensure failing CI

--- build.cmd
+++ build.cmd
@@ -8,7 +8,13 @@ if errorlevel 1 (
 
 pushd Content\
 call build.cmd
+if errorlevel 1 (
+  exit /b %errorlevel%
+)
 call build.cmd Clean
+if errorlevel 1 (
+  exit /b %errorlevel%
+)
 popd
 
 packages\build\FAKE\tools\FAKE.exe build.fsx %*
